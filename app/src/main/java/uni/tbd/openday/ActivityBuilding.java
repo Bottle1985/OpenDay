fce6999dae1089c3131068bf20214b2f006cb9f1
Bottle1985 <mrboongvn@yahoo.com.vn>
2020-02-14 02:34:06 +0700

    Added list building view

--- app/src/main/java/uni/tbd/openday/ActivityBuilding.java
+++ app/src/main/java/uni/tbd/openday/ActivityBuilding.java
@@ -5,13 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import uni.tbd.openday.Adapter.CustomAdapter;
 
 public class ActivityBuilding extends Activity {
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_building);
-
+        // Button template return
         Button next = (Button) findViewById(R.id.Button02);
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -20,5 +25,17 @@ public class ActivityBuilding extends Activity {
                 finish();
             }
         });
+        // List view function
+        final ListView list = findViewById(R.id.list);
+        ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
+        arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/", "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", "https://www.tutorialspoint.com/python/images/python-mini.jpg"));
+        arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", "https://www.tutorialspoint.com/cprogramming/images/c-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", "https://www.tutorialspoint.com/cplusplus/images/cpp-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
+        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
+        list.setAdapter(customAdapter);
+
     }
 }
---
+++ app/src/main/java/uni/tbd/openday/SubjectData.java
@@ -0,0 +1,12 @@
+package uni.tbd.openday;
+
+public class SubjectData {
+    public String SubjectName;
+    String Link;
+    public String Image;
+    public SubjectData(String subjectName, String link, String image) {
+        this.SubjectName = subjectName;
+        this.Link = link;
+        this.Image = image;
+    }
+}
