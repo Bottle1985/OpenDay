78dd0b91aa4ec5b8f41d47b175794f96c8968eff
Bottle1985 <mrboongvn@yahoo.com.vn>
2020-02-14 01:21:26 +0700

    Added template activity building

--- app/src/main/java/uni/tbd/openday/MainActivity.java
+++ app/src/main/java/uni/tbd/openday/MainActivity.java
@@ -2,13 +2,23 @@ package uni.tbd.openday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Open activity list building
+        Button next = (Button) findViewById(R.id.ButtonSoDo);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), ActivityBuilding.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
