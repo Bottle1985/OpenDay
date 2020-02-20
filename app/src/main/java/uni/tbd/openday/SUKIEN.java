995ba4df08a1cf1e90139e978a0cef598553cf20
ngtrongha <nt1712ngtrongha@gmail.com>
2020-02-20 10:08:00 +0700

    hanguyen
    tối ưu code

--- app/src/main/java/uni/tbd/openday/SUKIEN.java
+++ app/src/main/java/uni/tbd/openday/SUKIEN.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 
 import uni.tbd.openday.Activity.BuildingInfo;
 import uni.tbd.openday.Adapter.CustomAdapter;
-import uni.tbd.openday.Adapter.SuKienAdapter;
 
 public class SUKIEN extends AppCompatActivity {
 
@@ -23,13 +22,13 @@ public class SUKIEN extends AppCompatActivity {
         this.overridePendingTransition(R.anim.animation_enter,
                 R.anim.animation_leave);
         final ListView list = findViewById(R.id.list_sukien);
-        ArrayList<DataSuKien> arrayList = new ArrayList<DataSuKien>();
-        arrayList.add(new DataSuKien(getResources().getString(R.string.sukien0), "http://pou.edu.vn/news/thong-bao-ve-viec-sinh-vien-khong-den-truong-tranh-dich-benh-covid-19.788"));
-        arrayList.add(new DataSuKien(getResources().getString(R.string.sukien1), "http://pou.edu.vn/news/giang-vien-han-quoc-cong-tac-voi-nganh-dong-phuong-hoc.787"));
-        arrayList.add(new DataSuKien(getResources().getString(R.string.sukien2), "http://pou.edu.vn/news/dai-hoc-thai-binh-duong-cho-sinh-vien-nghi-den-16-2-2020.784"));
+        ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
+        arrayList.add(new SubjectData(getResources().getString(R.string.sukien0), "http://pou.edu.vn/news/thong-bao-ve-viec-sinh-vien-khong-den-truong-tranh-dich-benh-covid-19.788", R.drawable.sukien0));
+        arrayList.add(new SubjectData(getResources().getString(R.string.sukien1), "http://pou.edu.vn/news/giang-vien-han-quoc-cong-tac-voi-nganh-dong-phuong-hoc.787", R.drawable.sukien1));
+        arrayList.add(new SubjectData(getResources().getString(R.string.sukien2), "http://pou.edu.vn/news/dai-hoc-thai-binh-duong-cho-sinh-vien-nghi-den-16-2-2020.784", R.drawable.sukien2));
 
-        SuKienAdapter suKienAdapter = new SuKienAdapter(this, arrayList);
-        list.setAdapter(suKienAdapter);
+        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
+        list.setAdapter(customAdapter);
 
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
