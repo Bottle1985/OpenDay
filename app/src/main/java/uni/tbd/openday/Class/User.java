8e8451b5bbe9be08b2f9370aa581954028640eae
Bottle1985 <mrboongvn@yahoo.com.vn>
2020-09-28 13:14:15 +0700

    - Modified for user info

--- app/src/main/java/uni/tbd/openday/Class/User.java
+++ app/src/main/java/uni/tbd/openday/Class/User.java
@@ -5,8 +5,20 @@ public class User {
     public String name;
     public String email;
     public String pass;
+    // Variable to store data corresponding
+    // to firstname keyword in database
+    private String firstname;
 
+    // Variable to store data corresponding
+    // to lastname keyword in database
+    private String lastname;
 
+    // Variable to store data corresponding
+    // to age keyword in database
+    private String age;
+
+    // Mandatory empty constructor
+    // for use of FirebaseUI
     public User() {
     }
 
@@ -14,5 +26,30 @@ public class User {
         this.name = name;
         this.email = email;
         this.pass = pass;
+    }
+    // Getter and setter method
+    public String getFirstname()
+    {
+        return firstname;
+    }
+    public void setFirstname(String firstname)
+    {
+        this.firstname = firstname;
+    }
+    public String getLastname()
+    {
+        return lastname;
+    }
+    public void setLastname(String lastname)
+    {
+        this.lastname = lastname;
+    }
+    public String getAge()
+    {
+        return age;
+    }
+    public void setAge(String age)
+    {
+        this.age = age;
     }
 }
