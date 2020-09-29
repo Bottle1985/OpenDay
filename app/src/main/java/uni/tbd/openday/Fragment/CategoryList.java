871abdf72e6e01aac3ae0bbf25d004d0e07f6c92
Bottle1985 <mrboongvn@yahoo.com.vn>
2020-09-29 10:55:30 +0700

    Update CategoryList.java

--- app/src/main/java/uni/tbd/openday/Fragment/CategoryList.java
+++ app/src/main/java/uni/tbd/openday/Fragment/CategoryList.java
@@ -5,14 +5,69 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import uni.tbd.openday.R;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import uni.tbd.openday.Adapter.UserAdapter;
+import uni.tbd.openday.Class.User;
+import uni.tbd.openday.R;
 
 public class CategoryList extends Fragment {
+    private RecyclerView recyclerView;
+    UserAdapter adapter; // Create Object of the Adapter class
+    DatabaseReference mbase; // Create object of the
+    // Firebase Realtime Database
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_list, container, false);
+        View view =  inflater.inflate(R.layout.fragment_category_list, container, false);
+        // Create a instance of the database and get
+        // its reference
+        mbase                = FirebaseDatabase.getInstance().getReference();
+
+        recyclerView = getView().findViewById(R.id.recyclerView);
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(getContext()));
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<User> options
+                = new FirebaseRecyclerOptions.Builder<User>()
+                .setQuery(mbase, User.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new UserAdapter(options);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stoping of the activity
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        adapter.stopListening();
     }
 }
